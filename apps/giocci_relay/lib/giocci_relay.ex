20c0ddfa481bb683464d7456b5000bd43042dd02
Ryota Kinukawa <kinukawa@tombo-works.com>
2025-12-01 13:35:28 +0900

    Add `apps/giocci_relay` by `mix new giocci_relay --sup` under apps

---
+++ apps/giocci_relay/lib/giocci_relay.ex
@@ -0,0 +1,18 @@
+defmodule GiocciRelay do
+  @moduledoc """
+  Documentation for `GiocciRelay`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GiocciRelay.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/giocci_relay/test/giocci_relay_test.exs
@@ -0,0 +1,8 @@
+defmodule GiocciRelayTest do
+  use ExUnit.Case
+  doctest GiocciRelay
+
+  test "greets the world" do
+    assert GiocciRelay.hello() == :world
+  end
+end
