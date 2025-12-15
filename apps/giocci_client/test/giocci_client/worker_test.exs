7292179f8dacb4a55283219732af0c7b865019c8
Ryota Kinukawa <kinukawa@tombo-works.com>
2025-12-15 14:52:42 +0900

    Chore

--- apps/giocci_client/test/giocci_client/worker_test.exs
+++ apps/giocci_client/test/giocci_client/worker_test.exs
@@ -4,10 +4,6 @@ defmodule GiocciClient.WorkerTest do
   setup do
     _pid = start_supervised!({GiocciClient.Worker, [client_name: "giocci_client"]})
 
-    on_exit(fn ->
-      :ok = Application.delete_env(:giocci_client, :client_name)
-    end)
-
     :ok
   end
 
