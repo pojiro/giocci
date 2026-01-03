8e38b81ac544951d5ae381f1b1d5795b033033cd
Ryota Kinukawa <kinukawa@tombo-works.com>
2026-01-03 11:23:31 +0900

    Update giocci_engine.exs

--- apps/giocci_engine/config/giocci_engine.exs
+++ apps/giocci_engine/config/giocci_engine.exs
@@ -3,3 +3,4 @@ import Config
 
 config :giocci_engine, zenoh_config_file_path: "/root/giocci_engine/zenoh.json"
 config :giocci_engine, engine_name: "giocci_engine"
+config :giocci_engine, relay_name: "giocci_relay"
