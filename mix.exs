be854b39f6eccf950f1b0bd177fccaccbcf30c94
Ryota Kinukawa <kinukawa@tombo-works.com>
2026-01-07 14:25:09 +0900

    Add dialyxir to deps

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule Giocci.MixProject do
   # Run "mix help deps" for examples and options.
   defp deps do
     [
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 end
