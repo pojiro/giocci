62ab381aad39f79d65d17ad738e72cbb33be32aa
Ryota Kinukawa <kinukawa@tombo-works.com>
2026-01-13 16:30:31 +0900

    Elixir によるログファイル出力をやめ、 `docker logs` を使うようにする

--- config/runtime.exs
+++ config/runtime.exs
@@ -3,13 +3,3 @@ import Config
 config :logger, :default_formatter,
   colors: [enabled: false],
   format: "\n$date $time $metadata[$level] $message\n"
-
-config :logger, :default_handler,
-  config: [
-    file: ~c"log/giocci.log",
-    filesync_repeat_interval: 5000,
-    file_check: 5000,
-    max_no_bytes: 10_000_000,
-    max_no_files: 5,
-    compress_on_rotate: true
-  ]
