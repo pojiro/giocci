13d953df01cf72d6410d7c994f6b7421529f9b2b
Ryota Kinukawa <kinukawa@tombo-works.com>
2026-01-14 14:26:28 +0900

    Update giocci_engine/Dockerfile

--- apps/giocci_engine/config/giocci_engine.exs
+++ apps/giocci_engine/config/giocci_engine.exs
@@ -1,6 +1,6 @@
 # This config is an example for Giocci Engine Docker image
 import Config
 
-config :giocci_engine, zenoh_config_file_path: "/root/giocci_engine/zenoh.json"
+config :giocci_engine, zenoh_config_file_path: "/app/zenoh.json"
 config :giocci_engine, engine_name: "giocci_engine"
 config :giocci_engine, relay_name: "giocci_relay"
